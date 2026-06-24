3bf12f3b435c6c646d1e0f63fc7b887ffc6a8a39
Sneha Datta <snehadatta431@gmail.com>
2026-06-24 09:08:48 +0530

    chore: update proxy target to production API URL in vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/products': {
-        target: 'http://localhost:3000',
+        target: 'https://codevector-task-au48.onrender.com/',
         changeOrigin: true,
       },
     },
